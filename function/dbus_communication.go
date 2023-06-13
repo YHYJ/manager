067f5ef5989e1f474f84d0e82eec12ffe2f1afd5
YJ-ArchLinux <yj1516268@outlook.com>
2023-06-13 14:12:34 +0800

    +：添加PolicyKit身份认证

---
+++ function/dbus_communication.go
@@ -0,0 +1,75 @@
+/*
+File: dbus_communication.go
+Author: YJ
+Email: yj1516268@outlook.com
+Created Time: 2023-06-13 13:10:41
+
+Description: D-Bus总线通信
+*/
+
+package function
+
+import (
+	"os"
+
+	"github.com/godbus/dbus/v5"
+)
+
+func PolicyKitAuthentication() {
+	// PolicyKit认证，通过D-Bus总线通信
+	// 1. 连接到系统总线SystemBus
+	systemBus, err := dbus.SystemBus()
+	if err != nil {
+		panic(err)
+	}
+
+	// 2. 获取PolicyKit授权代理对象
+	// Object方法的第一个参数是Bus name，第二个参数是Object path
+	policykitObject := systemBus.Object("org.freedesktop.PolicyKit1", "/org/freedesktop/PolicyKit1/Authority")
+	if policykitObject == nil {
+		panic("Unable to perform Polkit authentication")
+	}
+
+	// 3. 构建认证方法的参数('subject', 'action_id', 'details', 'flags', 'cancellation_id')
+	// 'subject'需要新建一个如下结构体，内容是本进程的信息，包含进程ID和启动时间
+	type Subject struct {
+		Type       string
+		Identifier map[string]dbus.Variant
+	}
+	subject := Subject{
+		Type: "unix-process",
+		Identifier: map[string]dbus.Variant{
+			"pid":        dbus.MakeVariant(uint32(os.Getpid())),
+			"start-time": dbus.MakeVariant(uint64(0)),
+		},
+	}
+	// 'action_id'是一个字符串，用于标识本次认证的目的
+	actionID := "org.freedesktop.policykit.exec"
+	// 'details'是一个map，用于描述本次认证的详细信息
+	// details := map[string]dbus.Variant{
+	details := map[string]string{}
+	// 'flags'是一个uint32，用于标识本次认证的类型
+	flags := uint32(1)
+	// 'cancellation_id'是一个字符串，作为本次认证取消的标识
+	cancellationID := ""
+
+	// 4. 创建认证结果的接收对象
+	var result dbus.Variant
+
+	// 5. 调用CheckAuthorization方法进行认证
+	err = policykitObject.Call(
+		"org.freedesktop.PolicyKit1.Authority.CheckAuthorization", 0,
+		subject, actionID, details, flags, cancellationID).Store(&result)
+	if err != nil {
+		panic(err)
+	}
+
+	// 6. 解析认证结果
+	// 认证成功时，result的值是一个结构体，包含认证成功的信息
+	value := result.Value().([]interface{})[0]
+	if value.(bool) != true {
+		panic("Error executing command as another user: Not authorized")
+	}
+
+	// 7. 认证成功，执行需要认证的操作
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/yhyj/manager
 go 1.20
 
 require (
+	github.com/godbus/dbus/v5 v5.1.0
 	github.com/pelletier/go-toml v1.9.5
 	github.com/spf13/cobra v1.7.0
 )
