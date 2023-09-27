28291e1252a090ce6abac8575a69f58d931d91d0
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2023-09-27 10:23:13 +0800

    +: install.sh —— manager一键安装脚本

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+: << !
+Name: install.sh
+Author: YJ
+Email: yj1516268@outlook.com
+Created Time: 2023-09-27 10:21:55
+
+Description: manager安装脚本
+
+Attentions:
+-
+
+Depends:
+-
+!
