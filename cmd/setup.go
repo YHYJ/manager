60f87cb29713e6818d98c13b837f62d78a44978d
YJ-ArchLinux-Master <yj1516268@outlook.com>
2023-10-07 19:10:30 +0800

    E: setup.go —— 新增chezmoi、cobra、go配置

--- cmd/setup.go
+++ cmd/setup.go
@@ -24,9 +24,12 @@ var setupCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		// 解析参数
 		allFlag, _ := cmd.Flags().GetBool("all")
+		chezmoiFlag, _ := cmd.Flags().GetBool("chezmoi")
+		cobraFlag, _ := cmd.Flags().GetBool("cobra")
 		dockerFlag, _ := cmd.Flags().GetBool("docker")
 		frpcFlag, _ := cmd.Flags().GetBool("frpc")
 		gitFlag, _ := cmd.Flags().GetBool("git")
+		goFlag, _ := cmd.Flags().GetBool("go")
 		npmFlag, _ := cmd.Flags().GetBool("npm")
 		pipFlag, _ := cmd.Flags().GetBool("pip")
 
@@ -39,9 +42,27 @@ var setupCmd = &cobra.Command{
 
 		// 根据参数执行操作
 		if allFlag {
-			dockerFlag, frpcFlag, gitFlag, npmFlag, pipFlag = true, true, true, true, true
+			chezmoiFlag, cobraFlag, dockerFlag, frpcFlag, gitFlag, goFlag, npmFlag, pipFlag = true, true, true, true, true, true, true, true
 		}
 
+		// 配置chezmoi
+		if chezmoiFlag {
+			errSubject = "chezmoi"
+			errInfo = function.WriteFile(function.ChezmoiConfigFile, function.ChezmoiConfig)
+			if errInfo != nil {
+				errReport = fmt.Sprintf("%s: %s\n", errSubject, errInfo.Error())
+				fmt.Printf("\x1b[31m%s\x1b[0m\n", errReport)
+			}
+		}
+		// 配置cobra
+		if cobraFlag {
+			errSubject = "cobra"
+			errInfo = function.WriteFile(function.CobraConfigFile, function.CobraConfig)
+			if errInfo != nil {
+				errReport = fmt.Sprintf("%s: %s\n", errSubject, errInfo.Error())
+				fmt.Printf("\x1b[31m%s\x1b[0m\n", errReport)
+			}
+		}
 		// 配置docker
 		if dockerFlag {
 			errSubject = "docker"
@@ -64,6 +85,15 @@ var setupCmd = &cobra.Command{
 		if gitFlag {
 			errSubject = "git"
 			errInfo = function.WriteFile(function.GitConfigFile, function.GitConfig)
+			if errInfo != nil {
+				errReport = fmt.Sprintf("%s: %s\n", errSubject, errInfo.Error())
+				fmt.Printf("\x1b[31m%s\x1b[0m\n", errReport)
+			}
+		}
+		// 配置golang
+		if goFlag {
+			errSubject = "go"
+			errInfo = function.WriteFile(function.GoConfigFile, function.GoConfig)
 			if errInfo != nil {
 				errReport = fmt.Sprintf("%s: %s\n", errSubject, errInfo.Error())
 				fmt.Printf("\x1b[31m%s\x1b[0m\n", errReport)
@@ -92,9 +122,12 @@ var setupCmd = &cobra.Command{
 
 func init() {
 	setupCmd.Flags().BoolP("all", "", false, "Set up all programs/scripts")
+	setupCmd.Flags().BoolP("chezmoi", "", false, "Set up chezmoi")
+	setupCmd.Flags().BoolP("cobra", "", false, "Set up cobra-cli")
 	setupCmd.Flags().BoolP("docker", "", false, "Set up Docker Root Directory (need to be root)")
 	setupCmd.Flags().BoolP("frpc", "", false, "Set up frpc restart timing (need to be root)")
 	setupCmd.Flags().BoolP("git", "", false, "Set up git and generate SSH keys")
+	setupCmd.Flags().BoolP("go", "", false, "Set up golang")
 	setupCmd.Flags().BoolP("npm", "", false, "Set up the mirror source used by npm")
 	setupCmd.Flags().BoolP("pip", "", false, "Set up the mirror source used by pip")
 
--- function/setup.go
+++ function/setup.go
@@ -13,24 +13,29 @@ import "fmt"
 
 var (
 	varHome = GetVariable("HOME")
+	sep     = "    "
 
-	// pip配置文件内容
-	PipConfig     = "[global]\nindex-url = http://mirrors.aliyun.com/pypi/simple\ntrusted-host = mirrors.aliyun.com"
-	PipConfigFile = varHome + "/.config/pip/pip.conf"
+	// chezmoi
+	chezmoiSourceDir    = `sourceDir = "~/Documents/Repos/System/Profile"`
+	chezmoiAutoCommit   = "autoCommit = false"
+	chezmoiAutoPush     = "autoPush = false"
+	chezmoiConfigFormat = "%s\n[git]\n%s%s\n%s%s\n"
+	ChezmoiConfig       = fmt.Sprintf(chezmoiConfigFormat, chezmoiSourceDir, sep, chezmoiAutoCommit, sep, chezmoiAutoPush)
+	ChezmoiConfigFile   = varHome + "/.config/chezmoi/chezmoi.toml"
 
-	// npm配置文件内容
-	NpmConfig     = "registry=https://registry.npm.taobao.org"
-	NpmConfigFile = varHome + "/.npmrc"
+	// cobra配置
+	CobraConfig     = "author: YJ <yj1516268@outlook.com>\nlicense: GPLv3\nuseViper: false"
+	CobraConfigFile = varHome + "/.cobra.yaml"
 
-	// docker配置文件内容
+	// docker配置
 	DockerConfig     = "[Service]\nExecStart=\nExecStart=/usr/bin/dockerd --data-root=" + varHome + "/Documents/Docker/Root -H fd://"
 	DockerConfigFile = "/etc/systemd/system/docker.service.d/override.conf"
 
-	// frpc配置文件内容
+	// frpc配置
 	FrpcConfig     = "[Service]\nRestart=always\n"
 	FrpcConfigFile = "/etc/systemd/system/frpc.service.d/override.conf"
 
-	// git配置文件内容
+	// git配置
 	gitUserName          = "name = " + GetHostname()
 	gitUserEmail         = "email = yj1516268@outlook.com"
 	gitCoreEditor        = "editor = vim"
@@ -42,8 +47,19 @@ var (
 	gitFilterLfsSmudge   = "smudge = git-lfs smudge -- %f"
 	gitFilterLfsProcess  = "process = git-lfs filter-process"
 	gitFilterLfsRequired = "required = true"
-	sep                  = "    "
-	format               = "[user]\n%s%s\n%s%s\n[core]\n%s%s\n%s%s\n[merge]\n%s%s\n[color]\n%s%s\n[pull]\n%s%s\n[filter \"lfs\"]\n%s%s\n%s%s\n%s%s\n%s%s"
-	GitConfig            = fmt.Sprintf(format, sep, gitUserName, sep, gitUserEmail, sep, gitCoreEditor, sep, gitCoreAutoCRLF, sep, gitMergeTool, sep, gitColorUI, sep, gitPullRebase, sep, gitFilterLfsClean, sep, gitFilterLfsSmudge, sep, gitFilterLfsProcess, sep, gitFilterLfsRequired)
+	gitConfigFormat      = "[user]\n%s%s\n%s%s\n[core]\n%s%s\n%s%s\n[merge]\n%s%s\n[color]\n%s%s\n[pull]\n%s%s\n[filter \"lfs\"]\n%s%s\n%s%s\n%s%s\n%s%s\n"
+	GitConfig            = fmt.Sprintf(gitConfigFormat, sep, gitUserName, sep, gitUserEmail, sep, gitCoreEditor, sep, gitCoreAutoCRLF, sep, gitMergeTool, sep, gitColorUI, sep, gitPullRebase, sep, gitFilterLfsClean, sep, gitFilterLfsSmudge, sep, gitFilterLfsProcess, sep, gitFilterLfsRequired)
 	GitConfigFile        = varHome + "/.gitconfig"
+
+	// golang配置
+	GoConfig     = fmt.Sprintf("GO111MODULE=on\nGOBIN=%s/.go/bin\nGOPATH=%s/.go\nGOCACHE=%s/.cache/go/go-build\nGOMODCACHE=%s/.cache/go/pkg/mod", varHome, varHome, varHome, varHome)
+	GoConfigFile = varHome + "/.config/go/env"
+
+	// npm配置
+	NpmConfig     = "registry=https://registry.npm.taobao.org"
+	NpmConfigFile = varHome + "/.npmrc"
+
+	// pip配置
+	PipConfig     = "[global]\nindex-url = http://mirrors.aliyun.com/pypi/simple\ntrusted-host = mirrors.aliyun.com"
+	PipConfigFile = varHome + "/.config/pip/pip.conf"
 )
