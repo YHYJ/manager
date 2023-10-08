0ce3ff093a12f1c1d6539e34c568925cf5260128
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2023-10-08 09:31:13 +0800

    E: function/setup.go —— 更新配置项定义方式

--- function/setup.go
+++ function/setup.go
@@ -9,11 +9,16 @@ Description: 子命令`setup`功能函数
 
 package function
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 var (
-	varHome = GetVariable("HOME")
-	sep     = "    "
+	home     = GetVariable("HOME")
+	hostname = GetHostname()
+	email    = "yj1516268@outlook.com"
+	sep      = strings.Repeat(" ", 4)
 
 	// chezmoi
 	chezmoiSourceDir    = `"~/Documents/Repos/System/Profile"`
@@ -21,56 +26,68 @@ var (
 	chezmoiAutoPush     = false
 	chezmoiConfigFormat = "sourceDir = %s\n[git]\n%sautoCommit = %v\n%sautoPush = %v\n"
 	ChezmoiConfig       = fmt.Sprintf(chezmoiConfigFormat, chezmoiSourceDir, sep, chezmoiAutoCommit, sep, chezmoiAutoPush)
-	ChezmoiConfigFile   = varHome + "/.config/chezmoi/chezmoi.toml"
+	ChezmoiConfigFile   = home + "/.config/chezmoi/chezmoi.toml"
 
 	// cobra配置
 	cobraAuthor       = "YJ"
-	cobraEmail        = "yj1516268@outlook.com"
 	cobraLicense      = "GPLv3"
 	cobraUseViper     = false
 	cobraConfigFormat = "author: %s <%s>\nlicense: %s\nuseViper: %v\n"
-	CobraConfig       = fmt.Sprintf(cobraConfigFormat, cobraAuthor, cobraEmail, cobraLicense, cobraUseViper)
-	CobraConfigFile   = varHome + "/.cobra.yaml"
+	CobraConfig       = fmt.Sprintf(cobraConfigFormat, cobraAuthor, email, cobraLicense, cobraUseViper)
+	CobraConfigFile   = home + "/.cobra.yaml"
 
-	// docker配置
-	// docker service
-	DockerServiceConfig     = "[Service]\nExecStart=\nExecStart=/usr/bin/dockerd --data-root=" + varHome + "/Documents/Docker/Root -H fd://\n"
-	DockerServiceConfigFile = "/etc/systemd/system/docker.service.d/override.conf"
-	// docker mirrors
+	// docker配置 - docker service
+	dockerServiceExecStart    = "/usr/bin/dockerd"
+	dockerServiceDataRoot     = home + "/Documents/Docker/Root"
+	dockerServiceConfigFormat = "[Service]\nExecStart=\nExecStart=%s --data-root=%s -H fd://\n"
+	DockerServiceConfig       = fmt.Sprintf(dockerServiceConfigFormat, dockerServiceExecStart, dockerServiceDataRoot)
+	DockerServiceConfigFile   = "/etc/systemd/system/docker.service.d/override.conf"
+	// docker配置 - docker mirrors
 	dockerMirrorsRegistryMirrors = []string{`"https://docker.mirrors.ustc.edu.cn"`}
 	dockerMirrorsConfigFormat    = "{\n%s\"registry-mirrors\": %s\n}\n"
 	DockerMirrorsConfig          = fmt.Sprintf(dockerMirrorsConfigFormat, sep, dockerMirrorsRegistryMirrors)
 	DockerMirrorsConfigFile      = "/etc/docker/daemon.json"
 
 	// frpc配置
-	FrpcConfig     = "[Service]\nRestart=always\n"
-	FrpcConfigFile = "/etc/systemd/system/frpc.service.d/override.conf"
+	frpcRestart      = "always"
+	frpcConfigFormat = "[Service]\nRestart=%s\n"
+	FrpcConfig       = fmt.Sprintf(frpcConfigFormat, frpcRestart)
+	FrpcConfigFile   = "/etc/systemd/system/frpc.service.d/override.conf"
 
 	// git配置
-	gitUserName          = "name = " + GetHostname()
-	gitUserEmail         = "email = yj1516268@outlook.com"
-	gitCoreEditor        = "editor = vim"
-	gitCoreAutoCRLF      = "autocrlf = input"
-	gitMergeTool         = "tool = vimdiff"
-	gitColorUI           = "ui = true"
-	gitPullRebase        = "rebase = false"
-	gitFilterLfsClean    = "clean = git-lfs clean -- %f"
-	gitFilterLfsSmudge   = "smudge = git-lfs smudge -- %f"
-	gitFilterLfsProcess  = "process = git-lfs filter-process"
-	gitFilterLfsRequired = "required = true"
-	gitConfigFormat      = "[user]\n%s%s\n%s%s\n[core]\n%s%s\n%s%s\n[merge]\n%s%s\n[color]\n%s%s\n[pull]\n%s%s\n[filter \"lfs\"]\n%s%s\n%s%s\n%s%s\n%s%s\n"
-	GitConfig            = fmt.Sprintf(gitConfigFormat, sep, gitUserName, sep, gitUserEmail, sep, gitCoreEditor, sep, gitCoreAutoCRLF, sep, gitMergeTool, sep, gitColorUI, sep, gitPullRebase, sep, gitFilterLfsClean, sep, gitFilterLfsSmudge, sep, gitFilterLfsProcess, sep, gitFilterLfsRequired)
-	GitConfigFile        = varHome + "/.gitconfig"
+	gitCoreEditor        = "vim"
+	gitCoreAutoCRLF      = "input"
+	gitMergeTool         = "vimdiff"
+	gitColorUI           = true
+	gitPullRebase        = false
+	gitFilterLfsClean    = "git-lfs clean -- %f"
+	gitFilterLfsSmudge   = "git-lfs smudge -- %f"
+	gitFilterLfsProcess  = "git-lfs filter-process"
+	gitFilterLfsRequired = true
+	gitConfigFormat      = "[user]\n%sname = %s\n%semail = %s\n[core]\n%seditor = %s\n%sautocrlf = %s\n[merge]\n%stool = %s\n[color]\n%sui = %v\n[pull]\n%srebase = %v\n[filter \"lfs\"]\n%sclean = %s\n%ssmudge = %s\n%sprocess = %s\n%srequired = %v\n"
+	GitConfig            = fmt.Sprintf(gitConfigFormat, sep, hostname, sep, email, sep, gitCoreEditor, sep, gitCoreAutoCRLF, sep, gitMergeTool, sep, gitColorUI, sep, gitPullRebase, sep, gitFilterLfsClean, sep, gitFilterLfsSmudge, sep, gitFilterLfsProcess, sep, gitFilterLfsRequired)
+	GitConfigFile        = home + "/.gitconfig"
 
 	// golang配置
-	GoConfig     = fmt.Sprintf("GO111MODULE=on\nGOBIN=%s/.go/bin\nGOPATH=%s/.go\nGOCACHE=%s/.cache/go/go-build\nGOMODCACHE=%s/.cache/go/pkg/mod\n", varHome, varHome, varHome, varHome)
-	GoConfigFile = varHome + "/.config/go/env"
+	goGO111MODULE  = "on"
+	goGOBIN        = home + "/.go/bin"
+	goGOPATH       = home + "/.go"
+	goGOCACHE      = home + "/.cache/go/go-build"
+	goGOMODCACHE   = home + "/.cache/go/pkg/mod"
+	goConfigFormat = "GO111MODULE=%s\nGOBIN=%s\nGOPATH=%s\nGOCACHE=%s\nGOMODCACHE=%s\n"
+	GoConfig       = fmt.Sprintf(goConfigFormat, goGO111MODULE, goGOBIN, goGOPATH, goGOCACHE, goGOMODCACHE)
+	GoConfigFile   = home + "/.config/go/env"
 
 	// npm配置
-	NpmConfig     = "registry=https://registry.npm.taobao.org\n"
-	NpmConfigFile = varHome + "/.npmrc"
+	npmRegistry     = "https://registry.npm.taobao.org"
+	npmConfigFormat = "registry=%s\n"
+	NpmConfig       = fmt.Sprintf(npmConfigFormat, npmRegistry)
+	NpmConfigFile   = home + "/.npmrc"
 
 	// pip配置
-	PipConfig     = "[global]\nindex-url = http://mirrors.aliyun.com/pypi/simple\ntrusted-host = mirrors.aliyun.com\n"
-	PipConfigFile = varHome + "/.config/pip/pip.conf"
+	pipIndexUrl     = "https://mirrors.aliyun.com/pypi/simple"
+	pipTrustedHost  = "mirrors.aliyun.com"
+	pipConfigFormat = "[global]\nindex-url = %s\ntrusted-host = %s\n"
+	PipConfig       = fmt.Sprintf(pipConfigFormat, pipIndexUrl, pipTrustedHost)
+	PipConfigFile   = home + "/.config/pip/pip.conf"
 )
