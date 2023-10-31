d5131d901449479733b389d4dfcb8c73a720e530
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2023-10-31 16:20:25 +0800

    更新编译参数及其说明

--- function/version.go
+++ function/version.go
@@ -13,15 +13,22 @@ import "fmt"
 
 // 程序信息
 const (
-	Name    = "Manager"
-	Version = "v0.8.7"
-	Path    = "github.com/yhyj/manager"
+	name    = "Manager"
+	version = "v0.8.7"
+	project = "github.com/yhyj/manager"
+)
+
+// 编译信息
+var (
+	gitCommitHash string = "unknown"
+	buildTime     string = "unknown"
+	buildBy       string = "unknown"
 )
 
 func ProgramInfo(only bool) string {
-	programInfo := fmt.Sprintf("%s\n", Version)
+	programInfo := fmt.Sprintf("%s\n", version)
 	if !only {
-		programInfo = fmt.Sprintf("%s version %s\n", Name, Version)
+		programInfo = fmt.Sprintf("%s version: %s\nGit commit hash: %s\nBuilt on: %s\nBuilt by: %s\n", name, version, gitCommitHash, buildTime, buildBy)
 	}
 	return programInfo
 }
