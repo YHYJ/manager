979973ecb5f1d2fff14fdea2c5083cb203c44aa2
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2023-11-22 11:36:33 +0800

    添加Release安装部分的配置

--- cli/config.go
+++ cli/config.go
@@ -46,24 +46,28 @@ func GetTomlConfig(filePath string) (*toml.Tree, error) {
 func WriteTomlConfig(filePath string) (int64, error) {
 	// 根据系统不同决定某些参数
 	var (
-		installPath       = ""         // 定义在不同平台的安装路径
-		installSourceTemp = ""         // 定义在不同平台的Source存储目录
-		goNames           = []string{} // 定义在不同平台可用的程序
-		shellNames        = []string{} // 定义在不同平台可用的脚本
+		installPath        = ""         // 定义在不同平台的安装路径
+		installSourceTemp  = ""         // 定义在不同平台的Source安装方式的存储目录
+		installReleaseTemp = ""         // 定义在不同平台的Release安装方式的存储目录
+		goNames            = []string{} // 定义在不同平台可用的程序
+		shellNames         = []string{} // 定义在不同平台可用的脚本
 	)
 	if general.Platform == "linux" {
 		installPath = "/usr/local/bin"
 		installSourceTemp = "/tmp/manager/source"
+		installReleaseTemp = "/tmp/manager/release"
 		goNames = []string{"checker", "clone-repos", "eniac", "kbdstage", "manager", "rolling", "scleaner", "skynet"}
 		shellNames = []string{"collect-system", "configure-dtags", "py-virtualenv-tool", "save-docker-images", "sfm", "spacevim-update", "spider", "system-checkupdates", "trash-manager", "usb-manager"}
 	} else if general.Platform == "darwin" {
 		installPath = "/usr/local/bin"
 		installSourceTemp = "/tmp/manager/source"
+		installReleaseTemp = "/tmp/manager/release"
 		goNames = []string{"clone-repos", "manager", "skynet"}
 		shellNames = []string{"spacevim-update", "spider"}
 	} else if general.Platform == "windows" {
 		installPath = filepath.Join(general.GetVariable("ProgramFiles"), "Manager")
 		installSourceTemp = filepath.Join(general.UserInfo.HomeDir, "AppData", "Local", "Temp", "manager", "source")
+		installReleaseTemp = filepath.Join(general.UserInfo.HomeDir, "AppData", "Local", "Temp", "manager", "release")
 		goNames = []string{"skynet"}
 	}
 	// 定义一个map[string]interface{}类型的变量并赋值
@@ -73,18 +77,22 @@ func WriteTomlConfig(filePath string) (int64, error) {
 			"https_proxy": "", // HTTPS代理设置，例如"http://127.0.0.1:8080"
 		},
 		"install": map[string]interface{}{
-			"method":      "release",         // 安装方法，"release"或"source"代表安装预编译的二进制文件或自行从源码编译
-			"path":        installPath,       // 安装路径
-			"source_temp": installSourceTemp, // Source存储目录
+			"method":       "release",          // 安装方法，"release"或"source"代表安装预编译的二进制文件或自行从源码编译
+			"path":         installPath,        // 安装路径
+			"source_temp":  installSourceTemp,  // Source安装方式的基础存储目录
+			"release_temp": installReleaseTemp, // Release安装方式的基础存储目录
 			"go": map[string]interface{}{ // 基于go编写的程序的管理配置
 				"names":                    goNames,                         // 可用的程序列表
-				"generate_path":            "build",                         // 编译结果存储文件夹
+				"release_api":              "https://api.github.com",        // Release安装的API地址
+				"release_accept":           "application/vnd.github+json",   // Release安装的请求头参数
 				"source_url":               "https://github.com",            // 首选Source安装源地址
 				"source_api":               "https://api.github.com",        // 首选Source安装的API地址
 				"source_username":          "YHYJ",                          // 首选Source安装的用户名
 				"fallback_source_url":      "https://git.yj1516.top",        // 备用Source安装源地址
 				"fallback_source_api":      "https://git.yj1516.top/api/v1", // 备用Source安装的API地址
 				"fallback_source_username": "YJ",                            // 备用Source安装的用户名
+				"unzip_path":               "flowout",                       // 解压缩释出文件夹
+				"generate_path":            "build",                         // 编译结果存储文件夹
 				"completion_dir": []string{ // zsh的自动补全文件夹
 					filepath.Join(general.UserInfo.HomeDir, ".cache", "oh-my-zsh", "completions"),
 					filepath.Join(general.UserInfo.HomeDir, ".oh-my-zsh", "cache", "completions"),
