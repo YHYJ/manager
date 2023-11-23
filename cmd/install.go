33b395365d81e54602ff82f2dbd40ee5a194c364
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2023-11-23 17:19:27 +0800

    完善基于go的程序的Release安装方法（未完成）

--- cmd/install.go
+++ cmd/install.go
@@ -161,10 +161,11 @@ var installCmd = &cobra.Command{
 
 		// 字符串格式
 		var (
-			goLatestTagApiFormat             = "%s/repos/%s/%s/tags"           // 请求远端仓库最新 Tag 的 API
-			shellLatestHashApiFormat         = "%s/repos/%s/%s/contents/%s/%s" // 请求远端仓库最新脚本的 Hash 值的 API
-			shellGithubBaseDownloadUrlFormat = "%s/%s/%s/%s"                   // 远端仓库脚本基础下载地址（不包括在仓库路中的路径） - Github 格式
-			shellGiteaBaseDownloadUrlFormat  = "%s/%s/%s/raw/branch/%s"        // 远端仓库脚本基础下载地址（不包括在仓库路中的路径） - Gitea 格式
+			goLatestReleaseTagApiFormat      = "%s/repos/%s/%s/releases/latest" // 请求远端仓库最新 Tag 的 API - Release
+			goLatestSourceTagApiFormat       = "%s/repos/%s/%s/tags"            // 请求远端仓库最新 Tag 的 API - Source
+			shellLatestHashApiFormat         = "%s/repos/%s/%s/contents/%s/%s"  // 请求远端仓库最新脚本的 Hash 值的 API
+			shellGithubBaseDownloadUrlFormat = "%s/%s/%s/%s"                    // 远端仓库脚本基础下载地址（不包括在仓库路中的路径） - Github 格式
+			shellGiteaBaseDownloadUrlFormat  = "%s/%s/%s/raw/branch/%s"         // 远端仓库脚本基础下载地址（不包括在仓库路中的路径） - Gitea 格式
 		)
 
 		// 根据参数执行操作
@@ -186,10 +187,8 @@ var installCmd = &cobra.Command{
 			// 遍历所有脚本名
 			for _, name := range shellNames {
 				textLength := 0                                                                                                                            // 输出文本的长度
-				scriptLocalPath := filepath.Join(installSourceTemp, shellRepo, name.(string))                                                              // 脚本本地存储位置
 				shellGithubLatestHashApi := fmt.Sprintf(shellLatestHashApiFormat, shellGithubApi, shellGithubUsername, shellRepo, shellDir, name.(string)) // 请求远端仓库最新脚本的 Hash 值的 API
 				shellGiteaLatestHashApi := fmt.Sprintf(shellLatestHashApiFormat, shellGiteaApi, shellGiteaUsername, shellRepo, shellDir, name.(string))    // 请求远端仓库最新脚本的 Hash 值的 API
-				localProgram := filepath.Join(installPath, name.(string))                                                                                  // 本地程序路径
 				// 请求API
 				body, err := general.RequestApi(shellGithubLatestHashApi)
 				if err != nil {
@@ -207,6 +206,7 @@ var installCmd = &cobra.Command{
 					continue
 				}
 				// 获取本地脚本Hash值
+				localProgram := filepath.Join(installPath, name.(string))  // 本地程序路径
 				gitHashObjectArgs := []string{"hash-object", localProgram} // 本地程序参数
 				localHash, commandErr := general.RunCommandGetResult("git", gitHashObjectArgs)
 				// 比较远端和本地脚本Hash值
@@ -220,13 +220,12 @@ var installCmd = &cobra.Command{
 					shellGithubBaseDownloadUrl := fmt.Sprintf(shellGithubBaseDownloadUrlFormat, shellGithubRaw, shellGithubUsername, shellRepo, shellGithubBranch) // 脚本远端仓库基础地址
 					shellGiteaBaseDownloadUrl := fmt.Sprintf(shellGiteaBaseDownloadUrlFormat, shellGiteaRaw, shellGiteaUsername, shellRepo, shellGiteaBranch)      // 脚本远端仓库基础地址
 					shellUrlFile := filepath.Join(shellDir, name.(string))                                                                                         // 脚本在仓库中的路径
+					scriptLocalPath := filepath.Join(installSourceTemp, shellRepo, name.(string))                                                                  // 脚本本地存储位置
 					fileUrl := fmt.Sprintf("%s/%s", shellGithubBaseDownloadUrl, shellUrlFile)
-					_, err := cli.DownloadFile(fileUrl, scriptLocalPath)
-					if err != nil {
+					if _, err := cli.DownloadFile(fileUrl, scriptLocalPath); err != nil {
 						fmt.Printf(general.ErrorBaseFormat, err)
 						fileUrl := fmt.Sprintf("%s/%s", shellGiteaBaseDownloadUrl, shellUrlFile)
-						_, err = cli.DownloadFile(fileUrl, scriptLocalPath)
-						if err != nil {
+						if _, err = cli.DownloadFile(fileUrl, scriptLocalPath); err != nil {
 							fmt.Printf(general.ErrorBaseFormat, err)
 							continue
 						}
@@ -294,7 +293,87 @@ var installCmd = &cobra.Command{
 					fmt.Printf(general.ErrorBaseFormat, err)
 					return
 				}
-				// TODO: 待续 <23-11-23, YJ> //
+				// 遍历所有程序名
+				for _, name := range goNames {
+					textLength := 0                                                                                                       // 输出文本的长度
+					goGithubLatestReleaseTagApi := fmt.Sprintf(goLatestReleaseTagApiFormat, goGithubApi, goGithubUsername, name.(string)) // 请求远端仓库最新 Tag 的 API
+					// 请求API
+					body, err := general.RequestApi(goGithubLatestReleaseTagApi)
+					if err != nil {
+						fmt.Printf(general.ErrorBaseFormat, err)
+						continue
+					}
+					// 获取远端版本（用于Source安装）
+					remoteTag, err := general.GetLatestReleaseTag(body)
+					if err != nil {
+						fmt.Printf(general.ErrorBaseFormat, err)
+						continue
+					}
+					// 获取本地版本
+					localProgram := filepath.Join(installPath, name.(string)) // 本地程序路径
+					nameArgs := []string{"version", "--only"}                 // 本地程序参数
+					// localVersion, commandErr := general.RunCommandGetResult(localProgram, nameArgs)
+					localVersion, _ := general.RunCommandGetResult(localProgram, nameArgs)
+					// 比较远端和本地版本
+					if remoteTag == localVersion { // 版本一致，则输出无需更新信息
+						text := fmt.Sprintf(general.SliceTraverse3PSuffixFormat, "==>", " ", name.(string), " ", remoteTag, " ", latestVersionMessage)
+						fmt.Printf(text)
+						controlRegex := regexp.MustCompile(`\x1b\[[0-9;]*m`) // 去除控制字符，获取文本实际长度
+						textLength = len(controlRegex.ReplaceAllString(text, ""))
+					} else { // 版本不一致，则更新程序，并输出已更新信息
+						// 下载远端文件（如果Temp中已有远端文件则删除重新下载）
+						goReleaseTempDir := filepath.Join(installReleaseTemp, name.(string))
+						if general.FileExist(goReleaseTempDir) {
+							if err := os.RemoveAll(goReleaseTempDir); err != nil {
+								fmt.Printf(general.ErrorBaseFormat, err)
+							}
+						}
+						// 组装需要的文件的名称
+						fileName := general.FileName{}
+						fileName.ChecksumsFile = "checksums.txt"
+						fileType := func() string {
+							if general.Platform == "windows" {
+								return "zip"
+							}
+							return "tar.gz"
+						}()
+						fileName.ArchiveFile = fmt.Sprintf("%s_%s_%s_%s.%s", name.(string), remoteTag, general.Platform, general.Arch, fileType)
+						// 获取 Release 文件信息
+						filesInfo, err := general.GetReleaseFileInfo(body, fileName)
+						if err != nil {
+							fmt.Printf(general.ErrorBaseFormat, err)
+							continue
+						}
+						fmt.Printf(general.SliceTraverse2PSuffixNoNewLineFormat, "==>", " Download ", filesInfo.ChecksumsFileInfo.Name, " ", "from GitHub Release ")
+						checksumsLocalPath := filepath.Join(installReleaseTemp, name.(string), filesInfo.ChecksumsFileInfo.Name) // Checksums 文件本地存储位置
+						if _, err := cli.DownloadFile(filesInfo.ChecksumsFileInfo.DownloadUrl, checksumsLocalPath); err != nil {
+							fmt.Printf(general.ErrorSuffixFormat, "error", " -> ", err)
+							continue
+						} else {
+							fmt.Printf(general.SuccessFormat, "success")
+						}
+						fmt.Printf(general.SliceTraverse2PSuffixNoNewLineFormat, "==>", " Download ", filesInfo.ArchiveFileInfo.Name, " ", "from GitHub Release ")
+						archiveLocalPath := filepath.Join(installReleaseTemp, name.(string), filesInfo.ArchiveFileInfo.Name) // Release 文件本地存储位置
+						if _, err := cli.DownloadFile(filesInfo.ArchiveFileInfo.DownloadUrl, archiveLocalPath); err != nil {
+							fmt.Printf(general.ErrorSuffixFormat, "error", " -> ", err)
+							continue
+						} else {
+							fmt.Printf(general.SuccessFormat, "success")
+						}
+						// 进到下载的远端文件目录
+						if err := general.GoToDir(goReleaseTempDir); err != nil {
+							fmt.Printf(general.ErrorBaseFormat, err)
+							continue
+						}
+						// TODO: 校验 <23-11-23, YJ> //
+						// TODO: 解压 <23-11-23, YJ> //
+						// TODO: 安装 <23-11-23, YJ> //
+					}
+					dashes := strings.Repeat("-", textLength-1)  //组装分隔符（减去行尾换行符的一个长度）
+					fmt.Printf(general.LineHiddenFormat, dashes) // 美化输出
+					// 添加一个0.01秒的延时，使输出更加顺畅
+					time.Sleep(100 * time.Millisecond)
+				}
 			} else if strings.ToLower(installMethod) == "source" {
 				// 创建临时目录
 				if err := general.CreateDir(installSourceTemp); err != nil {
@@ -303,16 +382,14 @@ var installCmd = &cobra.Command{
 				}
 				// 遍历所有程序名
 				for _, name := range goNames {
-					textLength := 0                                                                                         // 输出文本的长度
-					compileProgram := filepath.Join(installSourceTemp, name.(string), goGeneratePath, name.(string))        // 编译生成的最新程序
-					goGithubLatestTagApi := fmt.Sprintf(goLatestTagApiFormat, goGithubApi, goGithubUsername, name.(string)) // 请求远端仓库最新 Tag 的 API
-					goGiteaLatestTagApi := fmt.Sprintf(goLatestTagApiFormat, goGiteaApi, goGiteaUsername, name.(string))    // 请求远端仓库最新 Tag 的 API
-					localProgram := filepath.Join(installPath, name.(string))                                               // 本地程序路径
+					textLength := 0                                                                                                     // 输出文本的长度
+					goGithubLatestSourceTagApi := fmt.Sprintf(goLatestSourceTagApiFormat, goGithubApi, goGithubUsername, name.(string)) // 请求远端仓库最新 Tag 的 API
+					goGiteaLatestSourceTagApi := fmt.Sprintf(goLatestSourceTagApiFormat, goGiteaApi, goGiteaUsername, name.(string))    // 请求远端仓库最新 Tag 的 API
 					// 请求API
-					body, err := general.RequestApi(goGithubLatestTagApi)
+					body, err := general.RequestApi(goGithubLatestSourceTagApi)
 					if err != nil {
 						fmt.Printf(general.ErrorBaseFormat, err)
-						body, err = general.RequestApi(goGiteaLatestTagApi)
+						body, err = general.RequestApi(goGiteaLatestSourceTagApi)
 						if err != nil {
 							fmt.Printf(general.ErrorBaseFormat, err)
 							continue
@@ -325,7 +402,8 @@ var installCmd = &cobra.Command{
 						continue
 					}
 					// 获取本地版本
-					nameArgs := []string{"version", "--only"} // 本地程序参数
+					localProgram := filepath.Join(installPath, name.(string)) // 本地程序路径
+					nameArgs := []string{"version", "--only"}                 // 本地程序参数
 					localVersion, commandErr := general.RunCommandGetResult(localProgram, nameArgs)
 					// 比较远端和本地版本
 					if remoteTag == localVersion { // 版本一致，则输出无需更新信息
@@ -378,6 +456,7 @@ var installCmd = &cobra.Command{
 							fmt.Printf(general.ErrorBaseFormat, unableToCompileMessage)
 						}
 						// 检测编译生成的程序是否存在
+						compileProgram := filepath.Join(installSourceTemp, name.(string), goGeneratePath, name.(string)) // 编译生成的程序
 						if general.FileExist(compileProgram) {
 							// 检测本地程序是否存在
 							if commandErr != nil { // 不存在，安装
