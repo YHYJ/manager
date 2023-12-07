156d4115e4fa09947366ebb40f2b10e6b1aa02e0
YJ-ArchLinux-Laptop <yj1516268@outlook.com>
2023-12-07 14:01:04 +0800

    v1.1.2

--- general/version.go
+++ general/version.go
@@ -17,7 +17,7 @@ import (
 
 const (
 	Name    string = "Manager"                 // 程序名
-	Version string = "v1.1.1"                  // 程序版本
+	Version string = "v1.1.2"                  // 程序版本
 	Project string = "github.com/yhyj/manager" // 项目地址
 )
 
